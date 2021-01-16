240701ff2e8c074b335a088c8c0cb0cba401aa7a
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-16 10:52:31 -0600

    created models and repositories for bean variety

---
+++ CoffeeShop/Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        SqlConnection Connection { get; }
+
+        void Add(BeanVariety variety);
+        void Delete(int id);
+        BeanVariety Get(int id);
+        List<BeanVariety> GetAll();
+        void Update(BeanVariety variety);
+    }
+}
